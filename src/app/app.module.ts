e812a7f2a22a567577de9f4100db608aad97ce08
Ben D <macuser@Macs-MacBook-Pro.local>
2019-03-07 16:55:11 -0500

    first ben commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { ProductsComponent } from './products/products.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { AlertService, AuthenticationService, UserService} from './_services';
+
 
 const appRoutes: Routes = [
-  { path: 'home', component: HomeComponent  },
+  { path: '', component: HomeComponent  },
   { path: 'about', component: AboutComponent },
   { path: 'products', component: ProductsComponent },
+  { path: 'login', component: LoginComponent },
 
+  { path: '**', redirectTo: ''}
  ];
 
 @NgModule({
@@ -19,7 +24,8 @@ const appRoutes: Routes = [
     AppComponent,
     AboutComponent,
     ProductsComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     RouterModule.forRoot(
@@ -27,8 +33,14 @@ const appRoutes: Routes = [
     ),
     BrowserModule,
     RouterModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    AlertService,
+    AuthenticationService,
+    UserService,
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
