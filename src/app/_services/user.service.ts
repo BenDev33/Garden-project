6534159e20336e6fd10b13db6869e409bf888e0f
Ben D <macuser@Macs-MacBook-Pro.local>
2019-03-07 21:06:38 -0500

    updating login form, unable to login still

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -9,22 +9,22 @@ export class UserService {
     constructor(private http: HttpClient) { }
 
     getAll() {
-        return this.http.get<User[]>(`${environment.apiUrl}/users`);
+        return this.http.get<User[]>(`${environment.apiUrl}/api/auth/login`);
     }
 
     getById(id: number) {
-        return this.http.get(`${environment.apiUrl}/users/` + id);
+        return this.http.get(`${environment.apiUrl}/api/auth/login` + id);
     }
 
     register(user: User) {
-        return this.http.post(`${environment.apiUrl}/users/register`, user);
+        return this.http.post(`${environment.apiUrl}/api/auth/login`, user);
     }
 
     update(user: User) {
-        return this.http.put(`${environment.apiUrl}/users/` + user.id, user);
+        return this.http.put(`${environment.apiUrl}/api/auth/login` + user.id, user);
     }
 
     delete(id: number) {
-        return this.http.delete(`${environment.apiUrl}/users/` + id);
+        return this.http.delete(`${environment.apiUrl}/api/auth/login` + id);
     }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,58 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { first } from 'rxjs/operators';
+import { AuthService } from '../auth.service';
 
-import { AlertService, AuthenticationService } from '../_services';
 
-@Component({templateUrl: 'login.component.html'})
+@Component({
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
+})
+
 export class LoginComponent implements OnInit {
-    loginForm: FormGroup;
-    loading = false;
-    submitted = false;
-    returnUrl: string;
-
-    constructor(
-        private formBuilder: FormBuilder,
-        private route: ActivatedRoute,
-        private router: Router,
-        private authenticationService: AuthenticationService,
-        private alertService: AlertService) {}
+    
+    // constructor(private Auth: AuthService) { }
 
     ngOnInit() {
-        this.loginForm = this.formBuilder.group({
-            username: ['', Validators.required],
-            password: ['', Validators.required]
-        });
 
-        // reset login status
-        this.authenticationService.logout();
-
-        // get return url from route parameters or default to '/'
-        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
     }
 
-    // convenience getter for easy access to form fields
-    get f() { return this.loginForm.controls; }
+    loginUser(event) {
+        event.preventDefault()
+        const target = event.target
+        const email = target.querySelector('email').value;
+        const password = target.querySelector('password').value;
 
-    onSubmit() {
-        this.submitted = true;
-
-        // stop here if form is invalid
-        if (this.loginForm.invalid) {
-            return;
-        }
-
-        this.loading = true;
-        this.authenticationService.login(this.f.username.value, this.f.password.value)
-            .pipe(first())
-            .subscribe(
-                data => {
-                    this.router.navigate([this.returnUrl]);
-                },
-                error => {
-                    this.alertService.error(error);
-                    this.loading = false;
-                });
+        this.Auth.getUserDetails(email, password);
+        console.log(email, password)
     }
 }
+
